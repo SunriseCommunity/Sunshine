6b08a5d6f3ab8fb981602b45fe66c3a160cd3576
richardscull <106016833+richardscull@users.noreply.github.com>
2025-07-05 10:14:57 +0300

    feat: Add new score listener;

---
+++ src/listeners/websocket/new-score.listener.ts
@@ -0,0 +1,61 @@
+import { ApplyOptions } from "@sapphire/decorators"
+import { container, Listener } from "@sapphire/framework"
+import { CUSTOM_EVENTS } from "../../lib/types/custom-events.types"
+import { getBeatmapById, type ScoreResponse } from "../../lib/types/api"
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js"
+
+@ApplyOptions<Listener.Options>({
+  event: CUSTOM_EVENTS.NewScore,
+  emitter: container.client.ws,
+})
+export class NewScoreListener extends Listener {
+  public async run(score: ScoreResponse) {
+    const { newScoresChannel } = this.container.config.ids
+    if (!newScoresChannel) return
+
+    this.container.client.logger.info(
+      `NewScoreListener: New score (id: ${score.id}) submitted, trying to send embed to scores channel.`,
+    )
+
+    const scoresChannel = await this.container.client.channels
+      .fetch(newScoresChannel.toString())
+      .catch(() =>
+        this.container.client.logger.error("NewScoreListener: Couldn't fetch scores channel"),
+      )
+
+    if (!scoresChannel || !scoresChannel.isSendable()) {
+      this.container.client.logger.error(
+        `NewScoreListener: Can't send new score embed. Check if scores channel ${newScoresChannel} exists.`,
+      )
+      return
+    }
+
+    const beatmap = await getBeatmapById({
+      path: {
+        id: score.beatmap_id,
+      },
+    })
+
+    if (!beatmap || beatmap.error) {
+      this.container.client.logger.error(
+        `NewScoreListener: Couldn't fetch score's (id: ${score.id}) beatmap (id: ${score.beatmap_id}).`,
+      )
+      return
+    }
+
+    const { embedPresets } = this.container.utilities
+    const scoreEmbed = await embedPresets.getScoreEmbed(score, beatmap.data, true)
+
+    const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setURL(`https://${this.container.config.sunrise.uri}/score/${score.id}`)
+        .setLabel("View score online")
+        .setStyle(ButtonStyle.Link),
+    )
+
+    await scoresChannel.send({
+      embeds: [scoreEmbed],
+      components: [buttons],
+    })
+  }
+}
