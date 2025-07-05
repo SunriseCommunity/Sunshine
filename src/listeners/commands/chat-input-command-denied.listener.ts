4a5513fd6fde27fa7fa969a28ac5eb46d441159e
richardscull <106016833+richardscull@users.noreply.github.com>
2025-07-05 10:15:07 +0300

    feat: Add ChatInputCommandDenied listener;

---
+++ src/listeners/commands/chat-input-command-denied.listener.ts
@@ -0,0 +1,35 @@
+import { ApplyOptions } from "@sapphire/decorators"
+import { Events, Identifiers, type ChatInputCommandDeniedPayload } from "@sapphire/framework"
+import { Listener, UserError } from "@sapphire/framework"
+import { MessageFlags, time } from "discord.js"
+
+@ApplyOptions<Listener.Options>({
+  name: Events.ChatInputCommandDenied,
+})
+export class ChatInputCommandDenied extends Listener {
+  public override async run(
+    { context, message: content, identifier }: UserError,
+    { interaction }: ChatInputCommandDeniedPayload,
+  ) {
+    if (Reflect.get(Object(context), "silent")) return
+
+    await interaction.deferReply({ flags: [MessageFlags.Ephemeral] })
+
+    if (identifier === Identifiers.PreconditionCooldown) {
+      const remaining = Object(context).remaining
+
+      const { embedPresets } = this.container.utilities
+
+      const cooldownEmbed = embedPresets.getErrorEmbed(
+        `Hold up! 😵‍💫 You are too fast for me! You can request again at ${time(
+          Math.floor((Date.now() + remaining) / 1000),
+          "R",
+        )}`,
+      )
+
+      return interaction.editReply({ embeds: [cooldownEmbed] })
+    }
+
+    return interaction.editReply({ content })
+  }
+}
