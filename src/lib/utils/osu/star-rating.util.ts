97eb53f659a2a49d9d6da289ff81c95e9fff6084
richardscull <106016833+richardscull@users.noreply.github.com>
2025-12-19 17:35:33 +0200

    feat: Return only 2 fraction of digits for star rating

--- src/lib/utils/osu/star-rating.util.ts
+++ src/lib/utils/osu/star-rating.util.ts
@@ -3,13 +3,13 @@ import { GameMode, type BeatmapResponse } from "../../types/api"
 export function getBeatmapStarRating(beatmap: BeatmapResponse, mode?: GameMode) {
   switch (mode ?? beatmap.mode) {
     case GameMode.STANDARD:
-      return beatmap.star_rating_osu
+      return beatmap.star_rating_osu.toFixed(2)
     case GameMode.TAIKO:
-      return beatmap.star_rating_taiko
+      return beatmap.star_rating_taiko.toFixed(2)
     case GameMode.CATCH_THE_BEAT:
-      return beatmap.star_rating_ctb
+      return beatmap.star_rating_ctb.toFixed(2)
     case GameMode.MANIA:
-      return beatmap.star_rating_mania
+      return beatmap.star_rating_mania.toFixed(2)
     default:
       return -1
   }
